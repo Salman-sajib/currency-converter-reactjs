0d2b645033a6591725df59c0fbb606f5349e0dad
Salman-sajib <sfsajib@gmail.com>
2024-02-05 22:37:15 +0600

    built the inputbox component with all the props and jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 function App() {
   return (
-    <div>
+    <div
+      className='w-full min-h-dvh flex flex-wrap justify-center items-center bg-cover bg-no-repeat'
+      style={{
+        backgroundImage: `url("./src/assets/anne-nygard-x07ELaNFt34-unsplash.jpg")`,
+      }}
+    >
       <h1 className='text-3xl'>Hello react</h1>
     </div>
   );
