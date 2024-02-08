7d6eec5a462cff5e050e5cf7eecba73c6c1f17c9
Salman-sajib <sfsajib@gmail.com>
2024-02-08 11:43:02 +0600

    changed the background image name

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
     <div
       className='w-full min-h-dvh flex flex-wrap justify-center items-center bg-cover bg-no-repeat'
       style={{
-        backgroundImage: `url("./src/Images/anne-nygard-x07ELaNFt34-unsplash.jpg")`,
+        backgroundImage: `url("./src/Images/bg-image.jpg")`,
       }}
     >
       <div className='w-full'>
